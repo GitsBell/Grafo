f16064793c7b7b200cae50cb198f4f40093489dc
GitsBell <33044771+GitsBell@users.noreply.github.com>
2017-10-24 16:27:16 -0300

    Add files via upload

---
+++ EjercicioSeccion2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <array>
+#include <string>
+using namespace std;
+
+array<double,8>distancias;
+void CaminoMasOptimo;
+
+int main(){
+	
+	CaminoMasOptimo()
+} 
+
+void CaminoMasOptimo(){
+	void RecopilarDatos();
+	void OrdenarDatos();
+	void MostrarEstructura();
+	
+	RecopilarDatos();
+	OrdenarDatos();
+	MostrarEstructura();
+}
+
+void RecopilarDatos(){
+	void Almacenar(double);
+	double dist;
+	cout<<"De punto de partida a punto de llegada nro "<<i+2;
+	for(int i=0;i<8;++i){
+		cin >> dist;
+		Almacenar(dist);
+	}
+}
+void Almacenar(int a){
+	
+	
+}
+
+
+
